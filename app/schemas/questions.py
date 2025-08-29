03887ca7b4d5c72e00aeade02f2271dd306a084b
Alexander-Pankow <pankowalexander49@gmail.com>
2025-08-29 21:13:39 +0200

    HW5 and HW6

--- app/schemas/questions.py
+++ app/schemas/questions.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional
-from app.schemas.category import CategoryResponse
+
+
+
+class CategoryBase(BaseModel):
+    name: str = Field(..., min_length=2, description="Category name")
 
 class QuestionCreate(BaseModel):
     question: str = Field(..., min_length=10, max_length=100)
-    category_id: Optional[int] = Field(None, description="Category ID")
+    category_id: int = Field(..., description="Category ID")
 
 class QuestionResponse(BaseModel):
     id: int
     question: str = Field(..., min_length=10, max_length=100)
-    category: Optional[CategoryResponse] = None
+    category: str = Field(..., min_length=10, max_length=100)
 
     model_config = ConfigDict(
         from_attributes=True
